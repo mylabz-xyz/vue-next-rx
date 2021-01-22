32b919c0b83fd0a680f6e29ed57adb9500e5f2b1
NOPR9D <issue@boucham-amine.fr>
2021-01-22 15:49:20 +0100

    :wastebasket: : remove unused files

--- get-updated-badge-info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "collecting stas for badges"
-
-commits=`git rev-list --all --count`
-latest_release_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo "{\"commits\":\"$commits\", \"release_tag"\:\"$latest_release_tag\"}" > badges.json
